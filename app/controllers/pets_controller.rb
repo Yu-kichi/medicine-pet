99d47593ba2ab3ee67270eb52fe62b9cb0776576
yamamotoyuki <yamamotoyama_yu_0125@yahoo.co.jp>
2021-06-06 16:08:17 +0900

    不要な部分を削除

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,61 +6,43 @@ class PetsController < ApplicationController
     @pets = Pet.all
   end
 
-  # GET /pets/1 or /pets/1.json
   def show
   end
 
-  # GET /pets/new
   def new
     @pet = Pet.new
   end
 
-  # GET /pets/1/edit
+  def create
+    @pet = Pet.new(pet_params)
+    if @pet.save
+      redirect_to @pet, notice:  "Pet was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   def edit
   end
 
-  # POST /pets or /pets.json
-  def create
-    @pet = Pet.new(pet_params)
-
-    respond_to do |format|
-      if @pet.save
-        format.html { redirect_to @pet, notice: "Pet was successfully created." }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /pets/1 or /pets/1.json
   def update
-    respond_to do |format|
-      if @pet.update(pet_params)
-        format.html { redirect_to @pet, notice: "Pet was successfully updated." }
-        format.json { render :show, status: :ok, location: @pet }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @pet.errors, status: :unprocessable_entity }
-      end
+    if @pet.update(pet_params)
+      redirect_to @pet, notice: "Pet was successfully updated."
+    else
+      render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE /pets/1 or /pets/1.json
   def destroy
     @pet.destroy
-    respond_to do |format|
-      format.html { redirect_to pets_url, notice: "Pet was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to pets_url, notice: "Pet was successfully destroyed."
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_pet
       @pet = Pet.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def pet_params
       params.require(:pet).permit(:name, :sex, :breed, :weight, :birthday)
     end

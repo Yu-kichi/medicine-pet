6e8cf2315fd00fb6f6cff88e2efdc05eb35cb86e
yamamotoyuki <yamamotoyama_yu_0125@yahoo.co.jp>
2021-06-09 01:20:23 +0900

    薬登録時にペット情報を渡せるように変更

--- app/controllers/medicine_notebook_controller.rb
+++ app/controllers/medicine_notebook_controller.rb
@@ -2,9 +2,7 @@
 
 class MedicineNotebookController < ApplicationController
   def index
-    # @pet = Pet.find(params[:pet_id])
     @pet = Pet.find_by(id: params[:pet_id])
     @clinics = @pet.clinics if @pet
-    @medicines = @clinics.medicines if @clinics
   end
 end

2a4d42f2368fc99195ed3cbf6b0ad67bca6bbf78
yamamotoyuki <yamamotoyama_yu_0125@yahoo.co.jp>
2021-06-15 03:02:05 +0900

    ルートを修正

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :pets do
     resources :medicine_notebook, only: %i(index)
     resources :clinics
+    resources :medicines
   end
 
   resources :clinics do

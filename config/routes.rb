db62b0e9459dead3e0c9ed8cdc77325935c96ba0
yamamotoyuki <yamamotoyama_yu_0125@yahoo.co.jp>
2021-06-16 16:40:16 +0900

    rubocop

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@
 
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    registrations: 'users/registrations',
-    sessions: 'users/sessions'
+    registrations: "users/registrations",
+    sessions: "users/sessions"
   }
 
   root "medicine_notebook#index"
--- db/migrate/20210615053225_devise_create_users.rb
+++ db/migrate/20210615053225_devise_create_users.rb
@@ -7,7 +7,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
       t.string :name, null: false
-      
+
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at

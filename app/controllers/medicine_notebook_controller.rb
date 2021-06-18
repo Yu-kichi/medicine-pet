e3a7c832b6d5440a516f0081de1a0e6a8d39ed98
yamamotoyuki <yamamotoyama_yu_0125@yahoo.co.jp>
2021-06-18 16:26:07 +0900

    ログイン直後にペットを登録していれば自動的にペットを選択している状態にした

--- app/controllers/medicine_notebook_controller.rb
+++ app/controllers/medicine_notebook_controller.rb
@@ -3,8 +3,11 @@
 class MedicineNotebookController < ApplicationController
   before_action :authenticate_user!
   def index
-    @pets = Pet.all
+    @pets = current_user.pets
     @pet = Pet.find_by(id: params[:pet_id])
+    if @pets && @pet.nil?
+      @pet = @pets.first
+    end
     @clinics = @pet.clinics if @pet
   end
 end

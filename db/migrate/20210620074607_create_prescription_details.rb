b34580df18b47b25ec90d6c003ede33f620d7325
yamamotoyuki <yamamotoyama_yu_0125@yahoo.co.jp>
2021-06-23 02:13:47 +0900

    処方箋と処方箋詳細のテーブルを作成。
    今まで薬と病院が持っていた情報をこちらに移行。

---
+++ db/migrate/20210620074607_create_prescription_details.rb
@@ -0,0 +1,10 @@
+class CreatePrescriptionDetails < ActiveRecord::Migration[6.0]
+  def change
+    create_table :prescription_details do |t|
+      t.references :prescription, foreign_key: true
+      t.references :medicine, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20210622053330_add_columns_to_prescription_details.rb
@@ -0,0 +1,7 @@
+class AddColumnsToPrescriptionDetails < ActiveRecord::Migration[6.0]
+  def change
+    add_column :prescription_details, :quantity, :integer 
+    add_column :prescription_details, :for_days, :integer 
+    add_column :prescription_details, :memo, :text 
+  end
+end

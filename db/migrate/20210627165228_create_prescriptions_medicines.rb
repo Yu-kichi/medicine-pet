cb6ea0cbf0514d0d3cfe47204947287a0d921dcd
yamamotoyuki <yamamotoyama_yu_0125@yahoo.co.jp>
2021-06-28 01:56:32 +0900

    処方箋と薬の中間テーブル作成

---
+++ db/migrate/20210627165228_create_prescriptions_medicines.rb
@@ -0,0 +1,12 @@
+class CreatePrescriptionsMedicines < ActiveRecord::Migration[6.0]
+  def change
+    create_table :prescriptions_medicines do |t|
+      t.integer :total_amount
+      t.integer :dose
+      t.references :medicine, foreign_key: true
+      t.references :prescription, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_27_164040) do
+ActiveRecord::Schema.define(version: 2021_06_27_165228) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -86,6 +86,17 @@ ActiveRecord::Schema.define(version: 2021_06_27_164040) do
     t.index ["pet_id"], name: "index_prescriptions_on_pet_id"
   end
 
+  create_table "prescriptions_medicines", force: :cascade do |t|
+    t.integer "total_amount"
+    t.integer "dose"
+    t.bigint "medicine_id"
+    t.bigint "prescription_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["medicine_id"], name: "index_prescriptions_medicines_on_medicine_id"
+    t.index ["prescription_id"], name: "index_prescriptions_medicines_on_prescription_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -105,4 +116,6 @@ ActiveRecord::Schema.define(version: 2021_06_27_164040) do
   add_foreign_key "pets", "users"
   add_foreign_key "prescriptions", "clinics"
   add_foreign_key "prescriptions", "pets"
+  add_foreign_key "prescriptions_medicines", "medicines"
+  add_foreign_key "prescriptions_medicines", "prescriptions"
 end

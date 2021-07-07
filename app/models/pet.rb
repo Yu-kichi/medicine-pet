b78af1a2bfc953a0d4cee1d950f186f4fa4495ad
yamamotoyuki <yamamotoyama_yu_0125@yahoo.co.jp>
2021-07-07 22:58:51 +0900

    rubocop

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -4,13 +4,13 @@ class Pet < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   has_many :prescriptions, dependent: :destroy
-  validates :image, 
+  validates :image,
     content_type: [:png, :jpg, :jpeg],
-    size: {less_than_or_equal_to: 10.megabytes},
-    dimension: {width: {max:2000}, height: {max: 2000}}
-  
+    size: { less_than_or_equal_to: 10.megabytes },
+    dimension: { width: { max: 2000 }, height: { max: 2000 } }
+
   attr_accessor :remove_image
- 
+
   before_save :remove_image_if_user_accept
 
   def remove_image_if_user_accept

ce5005ecdb3ab6b6097b5b11dbe32f36d99160cb
yamamotoyuki <yamamotoyama_yu_0125@yahoo.co.jp>
2021-07-19 16:56:29 +0900

    bulletが動くようにした、N＋1を解決。

--- app/controllers/medicine_notebook_controller.rb
+++ app/controllers/medicine_notebook_controller.rb
@@ -4,9 +4,10 @@ class MedicineNotebookController < ApplicationController
   before_action :authenticate_user!
   def index
     @pets = current_user.pets
-    @pet = Pet.includes(prescriptions: :prescriptions_medicines).find_by(id: params[:pet_id])
-    if @pets && @pet.nil?
-      @pet = @pets.first
+    if params[:pet_id]
+      @pet = Pet.includes(:prescriptions, prescriptions: [:clinic, prescriptions_medicines: :medicine]).find_by(id: params[:pet_id])
+    elsif @pets.present?
+      @pet = current_user.pets.includes(:prescriptions, prescriptions: [:clinic, prescriptions_medicines: :medicine]).first
     end
   end
 end

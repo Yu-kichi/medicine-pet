0d346db9e3b85ebb19170136c5cd9472a2edc25b
yamamotoyuki <yamamotoyama_yu_0125@yahoo.co.jp>
2021-08-31 02:29:33 +0900

    ヘッダーのハンバーガーメニューの部分をvue.jsで作成

---
+++ app/javascript/menuButton.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="ml-4" @click="showMenu = !showMenu">
+    <i class="fas fa-bars mt-5"></i>
+    <transition name="fade">
+      <div class="menu-bar" v-show="showMenu">
+        <ul class="mt-2 ml-3 mr-3">
+          <li class="pb-2 mt-2">
+            <p><a href="/pets" data-turbolinks="false"><i class="fas fa-cat mr-2"></i>ペット一覧</a></p>
+          </li>
+          <li class="pb-2 mt-2">
+            <p><a><i class="far fa-address-card mr-2"></i>ユーザー情報編集</a></p>
+          </li>
+          <li class="pb-2 mt-2">
+            <p>
+              <a href="/users/sign_out" data-method="delete" data-turbolinks="false">
+              <i class="fas fa-sign-out-alt mr-2"></i>Log out</a>
+            </p>
+          </li>
+        </ul> 
+      </div>
+    </transition> 
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return{
+      showMenu: false,
+    }
+  },
+}
+</script>

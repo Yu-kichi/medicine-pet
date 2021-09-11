d03cf99ecf86b3248986e39c870adfc57fc1ed3f
yamamotoyuki <yamamotoyama_yu_0125@yahoo.co.jp>
2021-09-11 14:47:04 +0900

    テスト用の一次変更を元に戻す

--- app/controllers/medicine_notebook_controller.rb
+++ app/controllers/medicine_notebook_controller.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 class MedicineNotebookController < ApplicationController
-  #before_action :authenticate_user!
+  before_action :authenticate_user!
   def index
-    # @pets = current_user.pets
-    # if params[:pet_id]
-    #   @pet = Pet.find_by(id: params[:pet_id])
-    # elsif @pets.present?
-    #   @pet = current_user.pets.first
-    # end
+    @pets = current_user.pets
+    if params[:pet_id]
+      @pet = Pet.find_by(id: params[:pet_id])
+    elsif @pets.present?
+      @pet = current_user.pets.first
+    end
   end
 end

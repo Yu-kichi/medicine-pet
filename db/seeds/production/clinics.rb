c4848ac0c5a9235ceba67801f817bcf599ecf4ba
yamamotoyuki <yamamotoyama_yu_0125@yahoo.co.jp>
2021-10-12 15:02:08 +0900

    本番環境にseedを投入できるようにした

---
+++ db/seeds/production/clinics.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+# データファイル一括取得
+file_names = []
+Dir.foreach("db/seeds/clinics") do |item|
+  next if (item == ".") || (item == "..")
+
+  file_names << ("db/seeds/clinics/#{item}")
+end
+
+file_names = file_names.sort
+# データ投入できるが時間はかかる。。
+file_names.each_with_index do |file, i|
+  File.foreach(file) do |line|
+    line = line.split(",")
+    Clinic.create(
+      name: line[0],
+      address: line[1],
+      telephone_number: line[2],
+      prefecture_id: i + 1,
+      user_id: 1
+    )
+  end
+end

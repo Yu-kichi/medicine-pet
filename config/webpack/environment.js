70fc2d8008bb1698bae24c066cd77b744d3b8018
yamamotoyuki <yamamotoyama_yu_0125@yahoo.co.jp>
2021-10-14 16:49:26 +0900

    webpackerの入れ直し

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,8 +1,9 @@
 const { environment } = require('@rails/webpacker')
+
 const { VueLoaderPlugin } = require('vue-loader')
-environment.plugins.prepend(
-  'VueLoaderPlugin',
-  new VueLoaderPlugin()
+  environment.plugins.prepend(
+    'VueLoaderPlugin',
+    new VueLoaderPlugin()
 )
 
 environment.loaders.prepend('vue', {

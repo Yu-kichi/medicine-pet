700f8ff64e4793daecd3ffd69117e4e58b245b4e
yamamotoyuki <yamamotoyama_yu_0125@yahoo.co.jp>
2021-11-25 17:44:08 +0900

    rubocop

--- app/helpers/pets_helper.rb
+++ app/helpers/pets_helper.rb
@@ -2,7 +2,7 @@
 
 module PetsHelper
   def birthday_to_age(birthday)
-    date = (Date.today.strftime('%Y%m%d').to_i - birthday.strftime('%Y%m%d').to_i) / 100
+    date = (Date.today.strftime("%Y%m%d").to_i - birthday.strftime("%Y%m%d").to_i) / 100
     year = date / 100
     month = date % 100
     "(#{year}歳#{month}ヶ月)"

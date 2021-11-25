ced698d5d4b8a77a40f5546ea69abd1ead6257b6
yamamotoyuki <yamamotoyama_yu_0125@yahoo.co.jp>
2021-11-25 17:38:43 +0900

    誕生日から年齢を算出するメソッド作成

--- app/helpers/pets_helper.rb
+++ app/helpers/pets_helper.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 module PetsHelper
+  def birthday_to_age(birthday)
+    date = (Date.today.strftime('%Y%m%d').to_i - birthday.strftime('%Y%m%d').to_i) / 100
+    year = date / 100
+    month = date % 100
+    "(#{year}歳#{month}ヶ月)"
+  end
 end

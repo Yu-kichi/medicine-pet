716bc6f5a728be9ffdac13f4cee1a2f3fee0e65b
yamamotoyuki <yamamotoyama_yu_0125@yahoo.co.jp>
2021-12-20 16:31:25 +0900

    不要なファイルの削除

--- app/helpers/prescriptions_medicines_helper.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module PrescriptionsMedicinesHelper
-  def medicine_options
-    Medicine.all.map do |medicine|
-      [medicine.name.to_s, medicine.id]
-    end
-  end
-end

79657e86d3c7148bdbdefaecfa344085254caa14
yamamotoyuki <yamamotoyama_yu_0125@yahoo.co.jp>
2022-02-10 22:20:00 +0900

    継承先でしか使わないためprotectedに変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  def set_pets
-    @pets = current_user.pets
-  end
+  protected
+    def set_pets
+      @pets = current_user.pets
+    end
 
-  def admin_login?
-    user_signed_in? && current_user.admin?
-  end
+    def admin_login?
+      user_signed_in? && current_user.admin?
+    end
 end
